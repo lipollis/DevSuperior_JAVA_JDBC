2d132e9e28e29b62f0129f9976dd77f78078541b
lipollis <aline.pollis@hotmail.com>
2022-08-20 00:27:52 -0300

    test exception deleteById

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -91,6 +91,10 @@ public class SellerDaoJDBC implements SellerDao {
             st.setInt(1, id);
 
             st.executeUpdate();
+//            int rows = st.executeUpdate();
+//            if (rows == 0){
+//                throw new DbException("ID not exist.");
+//            }
         }
         catch (SQLException e) {
             throw new DbException(e.getMessage());
